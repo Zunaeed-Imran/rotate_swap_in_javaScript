b2491265ea0d3e0ab9b57a67009eb5677d4b3cf3
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-03 16:55:20 +0600

    rotate array or swap in javaScript
    Write a function that rotates a list by k elements. For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2]. Try solving this without creating a copy of the list. How many swap or move operations do you need?

---
+++ rotateArray.js
@@ -0,0 +1,8 @@
+let array = [1, 2, 3, 4, 5, 6];
+for (let i = 3; i < 7; i++){
+    document.write(i+" ");
+}
+let array2 = [1, 2, 3, 4, 5, 6];
+for (let j = 1; j < 3; j++){
+  document.write(j+" ");
+}
